dc48c193be84369c4c4d1dc4796218f8d72a50fb
cocrodile <3082897304@qq.com>
2022-07-14 15:43:38 +0800

    首页开发

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,24 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
   </div>
 </template>
 <script>
 import NavBar from '@/components/common/navbar/NavBar.vue'
+import HomeSwiper from './childComps/HomeSwiper.vue'
+import RecommendView from './childComps/RecommendView.vue'
 import {getHmoeMultidata} from 'network/home'
+// import Swiper from '@/components/common/swiper/Swiper.vue'
+// import SwiperItem from '@/components/common/swiper/SwiperItem.vue'
+// import { Swiper,SwiperItem } from '@/components/common/swiper'
 export default {
   name: 'Home',
   components:{
-    NavBar
+    NavBar,
+    HomeSwiper,
+    RecommendView
   },
   data(){
     return{
@@ -21,8 +30,8 @@ export default {
     // 1.请求多个数据
     getHmoeMultidata().then(res => {
       // console.log(res);
-      this.banners=res.data.banner,
-      this.recommends=res.data.recommend
+      this.banners=res.data.banner.list,
+      this.recommends=res.data.recommend.list
     })
   }
 }
