66ff4c20516aaed4f5843cd5d0205e71920668f5
cocrodile <3082897304@qq.com>
2022-07-16 17:23:40 +0800

    首页开发

--- src/network/home.js
+++ src/network/home.js
@@ -2,8 +2,18 @@ import {
   request
 } from './request'
 
-export function getHmoeMultidata() {
+export function getHomeMultidata() {
   return request({
     url: '/home/multidata'
+  })
+}
+
+export function getHomeGoods(type, page) {
+  return request({
+    url: '/home/data',
+    params: {
+      type:type,
+      page:page
+    }
   })
 }
