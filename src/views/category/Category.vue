1dea642e4fbdbdb8e3b09584cc082647aeafa73f
cocrodile <3082897304@qq.com>
2022-07-17 22:53:14 +0800

    better-scroll

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,10 +1,29 @@
 <template>
-  <h2>分类</h2>
+  <div class="wrapper">
+    <ul class="content">
+      <li>分类列表1</li>
+      <li>分类列表1</li>
+      <li>分类列表1</li>
+      <li>分类列表1</li>
+    </ul>
+  </div>
 </template>
 <script>
+import BScroll from "better-scroll";
 export default {
-  name: 'Category'
-}
+  name: "Category",
+  data() {
+    return {};
+  },
+  created() {
+    let bs = new BScroll(".content", {});
+  },
+};
 </script>
 <style scoped>
+.wrapper {
+  height: 150px;
+  background-color: red;
+  overflow-y: scroll;
+}
 </style>
