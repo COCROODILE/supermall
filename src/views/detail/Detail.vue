bd0f32012c012531e952632152f29d356963105e
cocrodile <3082897304@qq.com>
2022-08-03 22:26:37 +0800

    获取详情页数据

---
+++ src/views/detail/Detail.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="detail">
+    <detail-nav-bar />
+    <detail-swiper :top-images="topImages" />
+  </div>
+</template>
+
+<script>
+import DetailNavBar from './childComps/DetailNavBar.vue'
+import DetailSwiper from './childComps/DetailSwiper.vue'
+import {getDetail} from 'network/detail' 
+export default {
+  name:'Datail',
+  components:{
+    DetailNavBar,
+    DetailSwiper
+  },
+  data(){
+    return{
+      iid:null,
+      topImages:[]
+    }
+  },
+  created(){
+    // 保存传入的iid
+    this.iid = this.$route.params.iid
+
+    // 根据iid请求详情数据
+    getDetail(this.iid).then(res => {
+      // console.log(res);
+      // 获取顶部的图片轮播数据
+      this.topImages=res.result.itemInfo.topImages
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
